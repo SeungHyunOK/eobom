e5a2dc4d244e4e75369965ea9c091ae1265fd0ad
OK <ohl2619@naver.com>
2025-02-11 23:14:46 +0900

    temp: 회원가입 페이지 폴더 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import NotFound from "./pages/NotFound/NotFound";
-import About from "./pages/About/About";
-import Header from "./components/common/Header";
 import "./global.css";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-100">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/notFound" element={<NotFound />} />
-      </Routes>
-    </div>
+    <>
+      <Signup />
+    </>
   );
 }
 
